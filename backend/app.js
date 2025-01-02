5a10a2d93ae4528db673c8ddba9054495ca557b4
Rayana Karthikeyan <rayanakarthikeyan@gmail.com>
2025-01-02 14:29:22 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+{
+  "name": "campx-backend",
+  "version": "1.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^7.3.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3"
+  }
+}
