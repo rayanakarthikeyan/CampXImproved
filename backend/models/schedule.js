f677e95131baa6c2c67e96911062481ef02201bf
Rayana Karthikeyan <rayanakarthikeyan@gmail.com>
2025-01-02 14:30:33 +0530

    Create schedule.js

---
+++ backend/models/schedule.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const scheduleSchema = new mongoose.Schema({
+  class: { type: String, required: true },
+  time: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Schedule", scheduleSchema);
